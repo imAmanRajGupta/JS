1f78041972e2b796c7d574e92d066488ac01ab19
imAmanRajGupta <amanraj2312@gmail.com>
2024-11-19 16:55:41 +0000

    variables done

---
+++ 01_basis/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=144553
+let accountEmail="amanraj2312@gmail.com"
+var accountPassword="12345"
+let accountState
+accountEmail="aman2312@gmail.com"
+accountPassword="212121"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountState])
