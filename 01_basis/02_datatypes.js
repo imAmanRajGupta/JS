8adff5f6e6bc51740a6d30729286685c4e5c1793
imAmanRajGupta <amanraj2312@gmail.com>
2024-11-19 17:22:53 +0000

    DataTypes Done

---
+++ 01_basis/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as new version
+
+//alert(3+3)  // we are using nodejs, not browser
+
+
+let name="aman"
+let number=56
+let flag=false
+//DataTypes
+// number =>2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol =>unique
+
+//object
+
+console.log(typeof null);
+console.log(typeof name);
+console.log(typeof undefined);
+
