78f1b55964852f33c4f23e6ff46fe7801fcd4fb0
imAmanRajGupta <amanraj2312@gmail.com>
2024-11-21 05:13:18 +0000

    DataType conversion Done

---
+++ 01_basis/03_conversionOperation.js
@@ -0,0 +1,46 @@
+let score="33"
+let score1="33abc"
+let score2=null
+let score3=undefined
+let score4=true
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let valueInNumber1=Number(score1)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber1);
+
+let valueInNumber2=Number(score2)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber2);
+
+let valueInNumber3=Number(score3)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber3);
+
+let valueInNumber4=Number(score4)
+//console.log(typeof valueInNumber4);
+//console.log(valueInNumber4);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 //false => 0
+
+
+let logIn="abc"
+let booleanLogIn=Boolean(logIn)
+//console.log(booleanLogIn);
+
+// 1 => true // 0 => false
+// "" => false  
+// "abc" => true
+
+let num=33
+let stringNum=String(num)
+console.log(typeof stringNum);
+console.log(stringNum);
+
