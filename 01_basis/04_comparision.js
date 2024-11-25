633bafcee49f5370bc8a94cb662a148a9d453948
imAmanRajGupta <amanraj2312@gmail.com>
2024-11-25 18:20:49 +0000

    comparison done

---
+++ 01_basis/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+//===
+// it checks the datatypes and value should be same
+console.log("2"==="2");
